25873f8bc71a264a57e8bf33903fb464a83df090
Andre Parron <andreparoon@gmail.com>
2024-03-26 19:27:54 -0300

    Configurando express-sessions com connect-mongo para autenticação de login.

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 import express from "express";
 import homeRoutes from "./src/routes/homeRoutes";
 import userRoutes from "./src/routes/userRoutes";
+import { GlobalMiddleware } from "./src/middlewares/middleware";
 
+//Imports necessários para configuração de sessions
+require('dotenv').config();
+import session from "express-session";
+import MongoStore from "connect-mongo";
 
 class App {
     constructor(){
@@ -13,6 +18,18 @@ class App {
     middlewares(){
         this.app.use(express.urlencoded({extended: true}));
         this.app.use(express.json());
+        //this.app.use(GlobalMiddleware);
+        //Configuração de express-sessions para autenticação de login
+        this.app.use(session({
+            secret: 'easease asfgeasgjoasiej0923',
+            store: MongoStore.create({ mongoUrl: process.env.CONNECTIONSTRING }),
+            resave: false,
+            saveUninitialized:false,
+            cookie:{
+                maxAge: 1000* 60 *60 *24 *7,
+                httpOnly:true
+            }
+          }));
     };
 
     routes(){
--- server.js
+++ server.js
@@ -2,14 +2,14 @@ import app from './app';
 require('dotenv').config();
 import mongoose from 'mongoose';
 
+//Configurando Conexao com banco de Dados
 mongoose.connect(process.env.CONNECTIONSTRING)
 .then(() => {
   console.log("Conectei a base de dados.");
   app.emit("pronto");
 }).catch(e => console.log('Erro Na Conexão do Banco de Dados.',e));
 
-
-
+//Iniciando servidor na porta 3001
 const port = 3001;
 app.listen(port, () => {
    console.log(`\nEscutando na porta ${port}`);
