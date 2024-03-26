e38572bdfdd21d0cd429b77131cc292af10b3581
Andre Parron <andreparoon@gmail.com>
2024-03-26 17:17:31 -0300

    Removendo código inutilizado

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -3,12 +3,6 @@ import homeModel from "../models/homeModel";
 class HomeController{
     async index(req,res){
         res.json({"TudoCerto":"true", "HomeController":"Funcionando"});
-       /* const insert = new homeModel({
-        "titulo":"Testando Inserção de dados.",
-         "nome":"Mongodb no praticando API." })
-        const infos = await insert.inserindoDados();
-        res.send(infos);
-        */
     };
 
 }
